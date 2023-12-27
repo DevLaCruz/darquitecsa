36af3c9a4a622f0b8445ea00a6a457e6c74b0b4f
DevLaCruz <97196517+DevLaCruz@users.noreply.github.com>
2023-12-26 19:50:39 -0500

    Update settings.py

--- darquitecsa/settings.py
+++ darquitecsa/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'darquitecsa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'Alejandro1234#',
+        'HOST': '161.132.39.76',
+        'PORT': '3306',  # Puede variar según la configuración
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
